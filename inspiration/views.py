f98b741d0b36509a9e1c98d67e2a158235a72cab
Alina <ralina_sha@mail.ru>
2020-04-02 17:03:49 +0300

    HTML page with list of poems

--- inspiration/views.py
+++ inspiration/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Poem
 
 # Create your views here.
 def poem_list(request):
-    return render(request, 'inspiration/poem_list.html', {})
+    poems = Poem.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'inspiration/poem_list.html', {'poems': poems})
