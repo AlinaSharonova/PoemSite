66c807d2c1681292db6487413631f8f30e90d343
Alina <ralina_sha@mail.ru>
2020-04-03 18:18:17 +0300

    Finish commit with DG

--- inspiration/urls.py
+++ inspiration/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.poem_list, name='poem_list'),
     path('poem/<int:pk>/', views.poem_detail, name='poem_detail'),
+    path('poem/new/', views.poem_new, name='poem_new'),
+    path('poem/<int:pk>/edit/', views.poem_edit, name='poem _edit'),
 ]
--- inspiration/views.py
+++ inspiration/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Poem
+from .forms import PoemForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def poem_list(request):
@@ -11,3 +13,30 @@ def poem_list(request):
 def poem_detail(request, pk):
     poem = get_object_or_404(Poem, pk=pk)
     return render(request, 'inspiration/poem_detail.html', {'poem': poem})
+
+def poem_new(request):
+    if request.method == "POST":
+        form = PoemForm(request.POST)
+        if form.is_valid():
+            poem = form.save(commit=False)
+            poem.author = request.user
+            poem.published_date = timezone.now()
+            poem.save()
+            return redirect('poem_detail', pk=poem.pk)
+    else:
+        form = PoemForm()
+    return render(request, 'inspiration/poem_edit.html', {'form': form})
+
+def poem_edit(request, pk):
+    poem = get_object_or_404(Poem, pk=pk)
+    if request.method == "POST":
+        form = PoemForm(request.POST, instance=poem)
+        if form.is_valid():
+            poem = form.save(commit=False)
+            poem.author = request.user
+            poem.published_date = timezone.now()
+            poem.save()
+            return redirect('poem_detail', pk=poem.pk)
+    else:
+        form = PoemForm(instance=poem)
+    return render(request, 'inspiration/poem_edit.html', {'form': form})
