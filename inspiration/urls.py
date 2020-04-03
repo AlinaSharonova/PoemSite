538918787a1d6e43edc233127fa83a0504b20da6
Alina <ralina_sha@mail.ru>
2020-04-03 04:27:54 +0300

    HTML CSS poem_detail pages

--- inspiration/urls.py
+++ inspiration/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.poem_list, name='poem_list'),
+    path('poem/<int:pk>/', views.poem_detail, name='poem_detail'),
 ]
--- inspiration/views.py
+++ inspiration/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Poem
 
@@ -6,3 +6,8 @@ from .models import Poem
 def poem_list(request):
     poems = Poem.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'inspiration/poem_list.html', {'poems': poems})
+
+
+def poem_detail(request, pk):
+    poem = get_object_or_404(Poem, pk=pk)
+    return render(request, 'inspiration/poem_detail.html', {'poem': poem})
